6d6c1ad2baf0b4a411caec39ff0e2c849279a778
farmersCod <96716901+farmersCod@users.noreply.github.com>
2023-04-24 11:59:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "https://restapi.amap.com/v3/weather/weatherInfo?key=55fe6d7eee5701df549772e201edb064&city=410482"
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temperature'])
+  weather = res['lives']['list'][0]
+  return weather['weather'],math.floor(weather['temperature'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
