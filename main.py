5858c7c0e5bf86eae3a165ff1579aeeb2e493173
farmersCod <96716901+farmersCod@users.noreply.github.com>
2023-04-24 11:42:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_weather():
   res = requests.get(url).json()
   weather = res['lives'][0]['weather']
   temp = res['lives'][0]['temperature']
-  return weather,temp
+  return weather['weather'], temp['temperature']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
