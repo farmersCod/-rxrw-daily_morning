d73e5ef0479c6a4a6143a812472aec2daa2e660d
farmersCod <96716901+farmersCod@users.noreply.github.com>
2023-04-24 11:13:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "https://restapi.amap.com/v3/weather/weatherInfo?key=55fe6d7eee5701df549772e201edb064&city=410482" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
